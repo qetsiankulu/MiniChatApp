d50b14c53af81b574144354de1c2b96a75990675
Qetsia Nkulu <qetsiankulu@gmail.com>
2025-09-15 09:35:42 -0700

    Initial Commit

---
+++ MiniChatApp/MiniChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChatAppApp.swift
+//  MiniChatApp
+//
+//  Created by Qetsia Nkulu  on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
